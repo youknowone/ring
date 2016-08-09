b027a22dca3580786dfe410b1bf7e71a631b4bc6
Jeong YunWon <jeong@youknowone.org>
2016-08-09 20:43:40 +0900

    decorator

--- cachain/ingredient.py
+++ cachain/ingredient.py
@@ -21,6 +21,22 @@ class CallableKey(Key):
         return self.key(**args)
 
 
+def _build_key(f, args, kwargs):
+    f_code = f.__code__
+    for i, arg in enumerate(args):
+        if i >= f_code.co_argcount:
+            raise TypeError(
+                '{} takes {} positional arguments but 4 were given'.format(
+                    f_code.co_name, f_code.co_argcount, len(args)))
+        varname = f_code.co_varnames[i]
+        if varname in kwargs:
+            raise TypeError(
+                "{}() got multiple values for argument '{}'".format(
+                    f_code.co_name, varname))
+        kwargs[varname] = arg
+    return kwargs
+
+
 class Ring(object):
 
     def __init__(self, storage, key):
@@ -34,40 +50,33 @@ class Ring(object):
                 raise TypeError
         self.key = key
 
-    @staticmethod
-    def _key_args(args, kwargs):
-        if args and kwargs:
-            raise TypeError
-        if args and len(args) > 1:
-            raise TypeError
-        if args:
-            key = args[0]
-        else:
-            key = kwargs
-        return key
+    def __call__(self, key=_build_key, expire=None):
+        def _wrapper(f):
+            def get_or_set(*args, **kwargs):
+                built_args = key(f, args, kwargs)
+                return self.get_or_set(f, **built_args)
+            return get_or_set
+        return _wrapper
 
-    def get(self, *args, **kwargs):
-        key_args = self._key_args(args, kwargs)
-        return self.get_by_key(key_args)
+    def get(self, **kwargs):
+        return self.get_by_key(kwargs)
 
     def get_by_key(self, key_args):
         built_key = self.key.build(key_args)
         return self.storage.get(built_key)
 
-    def set(self, _value, *args, **kwargs):
-        key_args = self._key_args(args, kwargs)
+    def set(self, _value, **kwargs):
         if callable(_value):
-            _value = _value(**key_args)
-        return self.set_by_key(_value, key_args)
+            _value = _value(**kwargs)
+        return self.set_by_key(_value, kwargs)
 
     def set_by_key(self, value, key_args):
         built_key = self.key.build(key_args)
         return self.storage.set(built_key, value)
 
-    def get_or_set(self, _value, *args, **kwargs):
-        key_args = self._key_args(args, kwargs)
-        value = self.get_by_key(key_args)
+    def get_or_set(self, _value, **kwargs):
+        value = self.get_by_key(kwargs)
         if value is None:
-            value = _value(**key_args)
-            self.set_by_key(value, key_args)
+            value = _value(**kwargs)
+            self.set_by_key(value, kwargs)
         return value
--- tests/test_cachain.py
+++ tests/test_cachain.py
@@ -14,13 +14,43 @@ def fx_ring():
 
 
 def test_ring_get_set(fx_ring):
-    assert fx_ring.get({'user_id': 1}) is None
-    fx_ring.set(lambda user_id: 100, {'user_id': 1})
+    assert fx_ring.get(user_id=1) is None
+    fx_ring.set(lambda user_id: 100, user_id=1)
 
 
 def test_ring_get_or_set(fx_ring):
-    assert fx_ring.get({'user_id': 1}) is None
-    assert fx_ring.get_or_set(lambda user_id: 100, {'user_id': 1}) == 100
-    assert fx_ring.get({'user_id': 1}) == 100
+    assert fx_ring.get(user_id=1) is None
+    assert fx_ring.get_or_set(lambda user_id: 100, user_id=1) == 100
+    assert fx_ring.get(user_id=1) == 100
 
 
+def test_decorator(fx_ring):
+
+    history = []
+
+    @fx_ring(expire=None)
+    def build_data(user_id):
+        history.append(user_id)
+        return {
+            'id': user_id,
+            'name': 'Name {}'.format(user_id),
+        }
+
+    u1 = build_data(user_id=1)
+    assert history == [1]
+    u2 = build_data(1)
+    assert history == [1]
+    assert u1 == u2 == {
+        'id': 1,
+        'name': 'Name 1'
+    }
+    u3 = build_data(3)
+    assert history == [1, 3]
+    u4 = build_data(3)
+    assert history == [1, 3]
+    assert u3 == u4 == {
+        'id': 3,
+        'name': 'Name 3'
+    }
+    u5 = build_data(1)
+    assert u1 == u5
