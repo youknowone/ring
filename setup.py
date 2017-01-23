a00b79138479ab79fdfb7f6b898948e45ad388f3
Jeong YunWon <jeong@youknowone.org>
2017-01-23 23:49:37 +0900

    0.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def get_readme():
 
 setup(
     name='ring',
-    version='0.1.0',
+    version='0.2.0',
     description='Generic cache decorator with built-in memcache & redis support.',
     long_description=get_readme(),
     author='Jeong YunWon',
