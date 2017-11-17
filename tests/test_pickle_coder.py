165a785b07a40a4c14bcb0cfa7a12dcc92782c2e
jowoojun <jowoojun@naver.com>
2017-11-17 22:49:53 +0900

    fix pylint error

--- tests/test_pickle_coder.py
+++ tests/test_pickle_coder.py
@@ -7,11 +7,12 @@ import pytest
 
 
 def test_coder_pickle():
-    assert pickle.dumps(1) == ring.coder.pickle.encode(1)
-    assert pickle.loads(pickle.dumps(1)) == ring.coder.pickle.decode(ring.coder.pickle.encode(1))
+    assert pickle.dumps(1, 0) == ring.coder.pickle.encode(1)
+    assert pickle.loads(pickle.dumps(1, 0)) == ring.coder.pickle.decode(ring.coder.pickle.encode(1))
 
-    assert pickle.dumps({'x': 1}) == ring.coder.pickle.encode({'x': 1})
-    assert pickle.loads(pickle.dumps({'x': 1})) == ring.coder.pickle.decode(ring.coder.pickle.encode({'x': 1}))
+    assert pickle.dumps({'x': 1}, 0) == ring.coder.pickle.encode({'x': 1})
+    assert pickle.loads(pickle.dumps({'x': 1}, 0)) == ring.coder.pickle.decode(ring.coder.pickle.encode({'x': 1}))
+
 
 def test_unexisting_coder():
     cache = {}
