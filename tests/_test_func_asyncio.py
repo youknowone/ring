f5429844db7e06aa4096d3b354d1927374de3ffa
Jeong YunWon <jeong@youknowone.org>
2020-01-28 09:52:50 +0900

    merge async def test and asyncio test

--- tests/_test_func_asyncio.py
+++ tests/_test_func_asyncio.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 import sys
 import shelve
+from typing import Optional
 
 import aiomcache
 import diskcache
@@ -485,3 +486,80 @@ async def test_forced_sync(synchronous_storage_and_ring):
     assert None is (await f.get('a'))
     assert 'a' == (await f('a'))
     assert 'a' == (await f.get('a'))
+
+
+@pytest.mark.asyncio
+async def test_async_def_func_method():
+    cache = {}
+
+    async def async_func(n):
+        return n
+
+    class A(object):
+        def __str__(self):
+            return 'A'
+
+        @ring.dict(cache)
+        async def method(self, a, b):
+            x = await async_func(100)
+            return base + a * x + b
+
+        @ring.dict(cache)
+        @classmethod
+        async def cmethod(cls, a, b):
+            x = await async_func(200)
+            return base + a * x + b
+
+    obj = A()
+
+    base = 10000
+    await obj.method.delete(1, 2)
+    value = await obj.method(1, 2)
+    assert value == 10102, value
+
+    await obj.cmethod.delete(1, 2)
+    value = await obj.cmethod(1, 2)
+    assert value == 10202, value
+
+
+@pytest.mark.parametrize('field,expected', [
+    (None, {'a': int, 'b': str, 'return': float}),
+    ('__call__', {'a': int, 'b': str, 'return': float}),
+    ('execute', {'a': int, 'b': str, 'return': float}),
+    ('get', {'a': int, 'b': str, 'return': Optional[float]}),
+    ('get_or_update', {'a': int, 'b': str, 'return': float}),
+    ('update', {'a': int, 'b': str, 'return': float}),
+    ('key', {'a': int, 'b': str, 'return': str}),
+    ('set', {'a': int, 'b': str, 'return': None}),
+    ('delete', {'a': int, 'b': str, 'return': None}),
+    ('touch', {'a': int, 'b': str, 'return': None}),
+
+])
+@pytest.mark.asyncio
+async def test_annotation(field, expected):
+
+    @ring.dict({})
+    def f(a: int, b: str) -> float:
+        pass
+
+    @ring.dict({})
+    async def g(a: int, b: str) -> float:
+        pass
+
+    if field is not None:
+        owner = getattr(f, field)
+    else:
+        owner = f
+
+    print('actual:', owner.__annotations__)
+    print('expected:', expected)
+    assert owner.__annotations__ == expected
+
+    if field is not None:
+        owner = getattr(g, field)
+    else:
+        owner = g
+
+    print('actual:', owner.__annotations__)
+    print('expected:', expected)
+    assert owner.__annotations__ == expected
--- tests/test_func_asyncio.py
+++ tests/test_func_asyncio.py
@@ -3,12 +3,8 @@ import sys
 import pytest
 
 
-if sys.version_info >= (3, 3):
+if sys.version_info >= (3, 5):
     from ._test_func_asyncio import *  # noqa
-
-    if sys.version_info >= (3, 5):
-        from ._test_func_async_def import *  # noqa
-
 else:
     @pytest.mark.skip
     def test_this_version_of_python_does_not_support_asyncio():
