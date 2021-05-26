022fc6d2b1120ae1e1bec531e0ede77b6e132e3d
Jeong YunWon <jeong@youknowone.org>
2021-05-26 12:30:04 +0900

    ring.typing

---
+++ ring/typing.py
@@ -0,0 +1,14 @@
+try:
+    from typing import Any, Optional, Tuple, List
+except ImportError:
+    class _Generic(object):
+        def __hash__(self):
+            return 0
+
+        def __getitem__(self, key):
+            return _Generic()
+
+    Optional = _Generic()
+    Tuple = _Generic()
+    List = _Generic()
+    Any = None
