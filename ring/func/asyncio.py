b46a6305dde57c4451c59ce63b2bafdd90264cab
yangroro <roro.coffee@kg.xyz>
2021-07-07 18:34:32 +0900

    Fix async wrap parameter
    - Remove useless loop parameter
    - Change name of executor to ring_executor

--- ring/func/asyncio.py
+++ ring/func/asyncio.py
@@ -76,11 +76,10 @@ def create_asyncio_factory_proxy(factory_table, *, support_asyncio):
 
 def async_wrap(func):
     @wraps(func)
-    async def run(*args, loop=None, executor=None, **kwargs):
-        if loop is None:
-            loop = asyncio.get_event_loop()
+    async def run(*args, ring_executor=None, **kwargs):
+        loop = asyncio.get_event_loop()
         pfunc = partial(func, *args, **kwargs)
-        return await loop.run_in_executor(executor, pfunc)
+        return await loop.run_in_executor(ring_executor, pfunc)
 
     return run
 
